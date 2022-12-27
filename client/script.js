cd3087efd75e422ce7682f6bf2f83b55c28041a7
Henry Diamond <henrydiamond94@gmail.com>
2022-12-27 23:34:51 +0400

    generate random number

--- client/script.js
+++ client/script.js
@@ -29,3 +29,10 @@ const typeText = (el, text) => {
     }
   }, 20);
 };
+
+const generateUniqueId = () => {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalString}`;
+};
